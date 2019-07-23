2eaeec41b48750e90c594362ed597f53c3afa634
Kaishan <kaishanpatel26@gmail.com>
2019-07-23 02:15:09 +0100

    commit

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://stark-escarpment-72168.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
